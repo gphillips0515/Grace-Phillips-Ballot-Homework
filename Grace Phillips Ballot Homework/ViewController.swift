666fc510deaf9b089816a49f6697561262faea40
Grace Phillips <g.phillips@setonhill.edu>
2020-03-22 18:25:23 -0400

    Initial Commit

---
+++ Grace Phillips Ballot Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grace Phillips Ballot Homework
+//
+//  Created by Grace Phillips on 3/22/20.
+//  Copyright © 2020 Grace Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
